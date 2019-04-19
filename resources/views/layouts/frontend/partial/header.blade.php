8f962f9cd69c640df5381ccb8ffc4fac5373c471
SungMinJung <cbedp4109@gmail.com>
2019-04-19 15:41:37 +0900

    header 로그아웃

--- resources/views/layouts/frontend/partial/header.blade.php
+++ resources/views/layouts/frontend/partial/header.blade.php
@@ -20,6 +20,20 @@
                     <li><a href="{{ route('author.dashboard') }}">Dashboard</a></li>
                 @endif
             @endguest
+
+            <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        
+                        <span>Logout</span>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+
         </ul><!-- main-menu -->
 
         <div class="src-area">
